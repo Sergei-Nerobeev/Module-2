4654372b1599058e2c9cecbf211f68e93f2de085
nero <sergeinerobeev@gmail.com>
2024-01-30 19:52:50 +0100

    Done task 1,2,3

--- src/com/javaacademy/homework3/task3/FortuneTelling.java
+++ src/com/javaacademy/homework3/task3/FortuneTelling.java
@@ -1,5 +1,5 @@
 package com.javaacademy.homework3.task3;
-
+@FunctionalInterface
 public interface FortuneTelling<T> {
   String predict(T input);
 }
--- src/com/javaacademy/homework3/task4/Runner.java
+++ src/com/javaacademy/homework3/task4/Runner.java
@@ -3,5 +3,6 @@ package com.javaacademy.homework3.task4;
 public class Runner {
   public static void main(String[] args) {
 
+
   }
 }
