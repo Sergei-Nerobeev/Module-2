9d6a3dcf275dd3bc6010294338ab356da8d2bdee
nero <sergeinerobeev@gmail.com>
2024-01-31 17:06:23 +0100

    Done task 1,2,3,4

---
+++ src/com/javaacademy/homework3/task4/Burning.java
@@ -0,0 +1,5 @@
+package com.javaacademy.homework3.task4;
+@FunctionalInterface
+public interface Burning {
+  void fireAll(Object object);
+}
--- src/com/javaacademy/homework3/task4/Runner.java
+++ src/com/javaacademy/homework3/task4/Runner.java
@@ -3,6 +3,11 @@ package com.javaacademy.homework3.task4;
 public class Runner {
   public static void main(String[] args) {
 
+    Burning bonfire = (tree) -> System.out.println("Желто-красный свет вокруг!");
+    Burning reactor = (uran) -> System.out.println("Зеленый свет вокруг!");
+
+    bonfire.fireAll("tree");
+    reactor.fireAll("uranium");
 
   }
 }
